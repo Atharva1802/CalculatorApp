f542864eee4bd0097f41ff9673a48b9ba818b2b4
Atharva Bhawsar <atharvabhawsar18@gmail.com>
2022-01-02 12:53:50 +0530

    Implied view binding with - functionality

--- app/src/main/java/at/app/calculatorapp/MainActivity.kt
+++ app/src/main/java/at/app/calculatorapp/MainActivity.kt
@@ -1,31 +1,33 @@
 package at.app.calculatorapp
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
+import at.app.calculatorapp.databinding.ActivityMainBinding
+
+import java.lang.ArithmeticException
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     var lastNumeric : Boolean = false       // Adding digit iteration bool
     var lastDot : Boolean = false           // Adding dot iteration bool
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 
     fun onDigit(view: View)
     {
-       tvInput.append((view as Button).text)
+       binding.tvInput.append((view as Button).text)
         lastNumeric = true
     }
 
     fun onClear (view: View)
     {
-        tvInput.setText("")
+        binding.tvInput.setText("")
         lastNumeric = false
         lastDot = false
     }
@@ -34,14 +36,41 @@ class MainActivity : AppCompatActivity() {
     {
         if (lastNumeric && !lastDot)
         {
-            tvInput.append(".")
+            binding.tvInput.append(".")
             lastDot = true
             lastNumeric = false
         }
     }
+
+    fun onEqual(view: View){
+        if (lastNumeric) {
+            var tvValue = binding.tvInput.text.toString()
+            var prefix = ""
+            try {
+                if (tvValue.startsWith("-")){
+                    prefix ="-"
+                    tvValue = tvValue.substring(1)
+                }
+                if (tvValue.contains('-')) {
+                    val splitValue = tvValue.split("-")
+
+                    var one = splitValue[0]
+                    var two = splitValue[1]     // Split to two before and after the minus
+
+                    if (!prefix.isEmpty()){
+                        one = prefix + one
+                    }
+
+                    binding.tvInput.text = (one.toDouble() - two.toDouble()).toString()
+                }
+            } catch (e: ArithmeticException) {
+                e.printStackTrace()
+            }
+        }
+    }
     fun onOperator(view: View){
-        if (lastNumeric &&!isOperatorAdded(tvInput.text.toString())){
-            tvInput.append((view as Button).text)
+        if (lastNumeric &&!isOperatorAdded(binding.tvInput.text.toString())){
+            binding.tvInput.append((view as Button).text)
             lastDot = false
             lastNumeric = false
         }
@@ -55,6 +84,5 @@ class MainActivity : AppCompatActivity() {
             value.contains("/") || value.contains("*") || value.contains("+")
                     || value.contains("-")
         }
-
     }
 }
